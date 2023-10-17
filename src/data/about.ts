8224eff8ac4fe07323058bed400fe6ab9a8ca088
Leo0114 <leonel0614@gmail.com>
2023-10-17 00:21:51 -0600

    smallbigChanges

---
+++ src/data/about.ts
@@ -0,0 +1,21 @@
+import one from "@assets/About/one.png";
+import two from "@assets/About/two.png";
+import thre from "@assets/About/tree.png";
+
+export const about = [
+  {
+    imagen: one,
+    title: "Servicio",
+    text: "Brindar oportunidades para que voluntarios de países desarrollados brinden un valioso servicio humanitario y se hagan amigos de niños y jóvenes desfavorecidos en México.",
+  },
+  {
+    imagen: two,
+    title: "Acceso",
+    text: "Ayudar a los niños desfavorecidos y marginados del centro-oeste de México a alcanzar su máximo potencial facilitando la educación y proporcionando apoyo material, actividades de enriquecimiento y servicios de salud que de otra manera no están disponibles para ellos.",
+  },
+  {
+    imagen: thre,
+    title: "Amistad",
+    text: "Crear y fomentar la amistad y el entendimiento entre niños y adultos de países desarrollados y sus pares en Colima, México.",
+  },
+];
